5722284f74bfef66231e61ea8a956e83c09db325
leadpogrommer <ilyamerzlakov@gmail.com>
2025-02-15 20:55:30 +0700

    SPI works???

--- main.c
+++ main.c
@@ -24,13 +24,8 @@ uint8_t nsu[] = {
 
 
 void matrix_send_raw(uint16_t data){
-    GPIOA_BSRR |= (1 << (16+4));
     SPI1_DR = data;
     while (!(SPI1_SR & SPI_SR_TXE) || (SPI1_SR & SPI_SR_BSY)); // wait for transfer to end
-//    delay(50); // WTF
-    GPIOA_BSRR |= (1 << 4);
-
-    delay(100);// TODO: remove this
 }
 
 void matrix_send(uint16_t addr, uint16_t data){
@@ -41,25 +36,21 @@ void matrix_send(uint16_t addr, uint16_t data){
 void matrix_draw_image(uint8_t* data){
     for(int i = 0; i < 8; i++){
         matrix_send(i+1, data[i]);
-
     }
 }
 
 void matrix_init(){
     RCC_AHB1ENR |= RCC_AHB1ENR_GPIOAEN; // enable gpio a
     RCC_APB2ENR |= RCC_APB2ENR_SPI1EN; // enable spi 1
-//    GPIOA_MODER |= (0b10 << 4*2) | (0b10 << 5*2) | (0b10 << 7*2);
-//    GPIOA_AFRL  |= (5 << 4*4)    | (5 << 5*4)    | (5 << 7*4)   ;
-    GPIOA_MODER |= (0b01 << 4*2) | (0b10 << 5*2) | (0b10 << 7*2);
-    GPIOA_AFRL  |=                 (5 << 5*4)    | (5 << 7*4)   ;
-
-    GPIOA_BSRR |= (1 << 4);
+    GPIOA_MODER |= (0b10 << 4*2) | (0b10 << 5*2) | (0b10 << 7*2);
+    GPIOA_AFRL  |= (5 << 4*4)    | (5 << 5*4)    | (5 << 7*4)   ;
 
 
-    SPI1_CR1 |= SPI_CR1_BIDIMODE_2LINE_UNIDIR | SPI_CR1_BIDIOE | SPI_CR1_DFF_16BIT | SPI_CR1_MSTR | SPI_CR1_BAUDRATE_FPCLK_DIV_256 | SPI_CR1_SSI | SPI_CR1_SSM; // WE GO SLOW
-//    SPI1_CR2 |= SPI_CR2_SSOE | SPI_CR2_FRF_TI_MODE;
+    SPI1_CR1 |= SPI_CR1_BIDIMODE_2LINE_UNIDIR | SPI_CR1_BIDIOE | SPI_CR1_DFF_16BIT | SPI_CR1_MSTR | SPI_CR1_BAUDRATE_FPCLK_DIV_2; // WE GO SLOW
+    SPI1_CR2 |= SPI_CR2_SSOE | SPI_CR2_FRF_TI_MODE;
     SPI1_CR1 |= SPI_CR1_SPE;
 
+    // IDK why, but sometimes it doesn't work
     matrix_send(0x09, 0); // no decode
     matrix_send(0x0C, 1); // no shutdown
     matrix_send(0x0b, 0b111); // no scan-limit
@@ -70,16 +61,15 @@ int main(){
     RCC_AHB1ENR |= RCC_AHB1ENR_GPIOCEN;
     GPIOC_MODER |= GPIO_MODE_OUTPUT << (13*2);
 
-    delay(10000);
     matrix_init();
+    matrix_draw_image(nsu);
 
 
     while (1){
-//        GPIOC_ODR |= 1 << 13;
-//        delay(DELAY);
-//        GPIOC_ODR &= ~(1 << 13);
-//        delay(DELAY);
-        matrix_draw_image(nsu);
+        GPIOC_ODR |= 1 << 13;
+        delay(DELAY);
+        GPIOC_ODR &= ~(1 << 13);
+        delay(DELAY);
 //        matrix_send_raw(0b1010000000000001);
     }
 }
