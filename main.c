289a6f705985d5a9433bd96e6318f4175d900b88
leadpogrommer <ilyamerzlakov@gmail.com>
2025-02-16 00:16:28 +0700

    WIP dma spi

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include "libopencm3/stm32/gpio.h"
 #include "libopencm3/stm32/rcc.h"
 #include "libopencm3/stm32/spi.h"
+#include "libopencm3/stm32/dma.h"
 
 #define DELAY 300000
 void delay(int d){
@@ -21,11 +22,9 @@ uint8_t nsu[] = {
     0b00000000
 };
 
-
-
 void matrix_send_raw(uint16_t data){
     SPI1_DR = data;
-    while (!(SPI1_SR & SPI_SR_TXE) || (SPI1_SR & SPI_SR_BSY)); // wait for transfer to end
+    while (!(SPI1_SR & SPI_SR_TXE)); // wait for transfer to end
 }
 
 void matrix_send(uint16_t addr, uint16_t data){
@@ -39,22 +38,40 @@ void matrix_draw_image(uint8_t* data){
     }
 }
 
+void matrix_send_dma(uint16_t *data, int len){
+    DMA2_S2CR &= ~DMA_SxCR_EN; // stop stream
+
+    DMA2_S2PAR = &SPI1_DR;
+    DMA2_S2M0AR = data;
+    DMA2_S2NDTR = len;
+
+
+    DMA2_S2CR = DMA_SxCR_DIR_MEM_TO_PERIPHERAL | DMA_SxCR_PL_LOW | DMA_SxCR_MSIZE_16BIT | DMA_SxCR_PSIZE_16BIT | DMA_SxCR_MINC;
+    DMA2_S2CR = DMA_SxCR_CHSEL(2);
+
+    DMA2_S2CR |= DMA_SxCR_EN;
+}
+
 void matrix_init(){
     RCC_AHB1ENR |= RCC_AHB1ENR_GPIOAEN; // enable gpio a
     RCC_APB2ENR |= RCC_APB2ENR_SPI1EN; // enable spi 1
+    RCC_AHB1ENR |= RCC_AHB1ENR_DMA2EN;
     GPIOA_MODER |= (0b10 << 4*2) | (0b10 << 5*2) | (0b10 << 7*2);
     GPIOA_AFRL  |= (5 << 4*4)    | (5 << 5*4)    | (5 << 7*4)   ;
 
 
     SPI1_CR1 |= SPI_CR1_BIDIMODE_2LINE_UNIDIR | SPI_CR1_BIDIOE | SPI_CR1_DFF_16BIT | SPI_CR1_MSTR | SPI_CR1_BAUDRATE_FPCLK_DIV_2; // WE GO SLOW
-    SPI1_CR2 |= SPI_CR2_SSOE | SPI_CR2_FRF_TI_MODE;
+    SPI1_CR2 |= SPI_CR2_SSOE | SPI_CR2_FRF_TI_MODE | SPI_CR2_TXDMAEN;
     SPI1_CR1 |= SPI_CR1_SPE;
 
     // IDK why, but sometimes it doesn't work
-    matrix_send(0x09, 0); // no decode
-    matrix_send(0x0C, 1); // no shutdown
-    matrix_send(0x0b, 0b111); // no scan-limit
-    matrix_send(0x0a, 1); // intensity
+    uint16_t data[] = {0xaba, 0b10100001};
+    matrix_send_dma(data, 2);
+
+//    matrix_send(0x09, 0); // no decode
+//    matrix_send(0x0C, 1); // no shutdown
+//    matrix_send(0x0b, 0b111); // no scan-limit
+//    matrix_send(0x0a, 1); // intensity
 }
 
 int main(){
@@ -62,7 +79,7 @@ int main(){
     GPIOC_MODER |= GPIO_MODE_OUTPUT << (13*2);
 
     matrix_init();
-    matrix_draw_image(nsu);
+//    matrix_draw_image(nsu);
 
 
     while (1){
